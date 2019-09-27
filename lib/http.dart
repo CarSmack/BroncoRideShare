6e6d4f1f5ca3e0a4e681e54efcb9ec630d506f7f
Dhairya <dhairyabarot10.com>
2019-09-26 17:23:55 -0700

    dhairya1

--- lib/http.dart
+++ lib/http.dart
@@ -1,34 +1,31 @@
 import 'package:http/http.dart' as http;
 
-import 'dart:async';
-import 'dart:convert';
+/*
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
-
-const baseUrl = "https://jsonplaceholder.typicode.com";
-
-class GetRequest {
-  static Future getUsers() {
-    var url = baseUrl + "/users";
-    return http.get(url);
+main(List<String> arguments) async {
+  // This example uses the Google Books API to search for books about http.
+  // https://developers.google.com/books/docs/overview
+  //var url = "https://www.googleapis.com/books/v1/volumes?q={http}";
+  var url = "https://www.cpp.edu/~alumni";
+  // Await the http get response, then decode the json-formatted responce.
+  var response = await http.get(url);
+  if (response.statusCode == 200) {
+    var jsonResponse = convert.jsonDecode(response.body);
+    var itemCount = jsonResponse['totalItems'];
+    print("Number of books about http: $itemCount.");
+  } else {
+    print("Request failed with status: ${response.statusCode}.");
   }
-}
-class User {
-  int id;
-  String name;
-  String email;
+}*/
+import 'package:http/http.dart' as http;
+main(List<String> arguments) async {
+  var url = 'https://www.cpp.edu/~alumni/';
+  var response = await http.post(
+      url, body: {'name': 'doodle', 'color': 'blue'});
+  print('Response status: ${response.statusCode}');
+  print('Response body: ${response.body}');
 
-  User(int id, String name, String email) {
-    this.id = id;
-    this.name = name;
-    this.email = email;
-  }
-
-  User.fromJson(Map json)
-      : id = json['id'],
-        name = json['name'],
-        email = json['email'];
-
-  Map toJson() {
-    return {'id': id, 'name': name, 'email': email};
-  }
+  print(await http.read('https://www.cpp.edu/~alumni/'));
 }
